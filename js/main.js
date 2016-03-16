30d500636618f0daafa60571a2019296a7aff5bb
joecih <joecih@gmail.com>
2016-03-16 14:42:25 -0400

    some final changes to navbar and responsiveness

--- js/main.js
+++ js/main.js
@@ -1,14 +1,32 @@
 $(document).ready(function() {
+
+    var winWidth = 0;
+    var winHeight = 0;
+
+
+    $(window).resize(function() {
+        winWidth = window.innerWidth;
+        winHeight = window.innerHeight;
+
+        if (winWidth <= 480) {
+            console.log("Win Width: " + winWidth);
+            $(".yt3").css("padding-top", "0");
+            $(".form-inline").css("padding-top","0");
+        }
+    });
+
     $("#home-sel").click(function() {
         $(".content-frame").load("views/page1.html");
         $(".sub-head h1").html("Home");
     });
-    
+
     $("#videos-sel").click(function() {
         $(".content-frame").load("views/page2.html");
-        $(".sub-head h1").html("Videos");     
+        $(".sub-head h1").html("Videos");
     });
-    
+
     $(".content-frame").load("views/page1.html");
     $(".sub-head h1").html("Home");
+
+
 });
