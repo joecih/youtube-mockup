c9f7d9d06c44f9caf005982e7663b751f977a16a
joecih <joecih@gmail.com>
2016-03-16 13:45:28 -0400

    Added fix in for switch H1 from 'Home' to 'Videos' depending on page loaded.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
     $("#home-sel").click(function() {
         $(".content-frame").load("views/page1.html");
+        $(".sub-head h1").html("Home");
     });
     
     $("#videos-sel").click(function() {
-        $(".content-frame").load("views/page2.html");      
+        $(".content-frame").load("views/page2.html");
+        $(".sub-head h1").html("Videos");     
     });
     
     $(".content-frame").load("views/page1.html");
+    $(".sub-head h1").html("Home");
 });
